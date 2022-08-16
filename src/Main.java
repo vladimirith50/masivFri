0823f9351dbd0b2d8518059ff6fa5c76fe28ea5b
vladimirith50 <vladivod.serg.45@mail.ru>
2022-08-16 16:07:34 +0300

    первый комит

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //задание 1
+        int nomer[] = new int[3];
+        nomer[0] = 1;
+        nomer[1] = 2;
+        nomer[2] = 3;
+
+        for (int i = 0; i < nomer.length; i++) {
+            System.out.println(nomer[1]);
+        }
+
+        double masiv[] = {1.57, 7.654, 9.986};
+        System.out.println(masiv[2]);
+
+        int zero[] = new int[10];
+        zero[2] = 15;
+        zero[4] = 8;
+        zero[7] = 4;
+        System.out.println("килограмм фруктов" + zero[2]);
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
